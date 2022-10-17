e694c3df12f0276390e4b1f65f0002477ecd06e6
Sergey Petrinich <petrinichshadow@gmail.com>
2022-10-17 22:32:20 +0300

    cleaned code \ deployed with firebase

---
+++ src/js/files/forms.js
@@ -0,0 +1,157 @@
+// Подключение функционала "Чертогов Фрилансера"
+// Подключение списка активных модулей
+// Вспомогательные функции
+import { _slideUp, _slideDown, _slideToggle } from "./functions.js";
+// Модуль прокрутки к блоку
+import { gotoBlock } from "./scroll/gotoblock.js";
+//================================================================================================================================================================================================================================================================================================================================
+
+// Работа с полями формы. Добавление классов, работа с placeholder
+export function formFieldsInit(options = { viewPass: false }) {
+    // Если включено, добавляем функционал "скрыть плейсходлер при фокусе"
+    const formFields = document.querySelectorAll("input[placeholder],textarea[placeholder]");
+    if (formFields.length) {
+        formFields.forEach((formField) => {
+            if (!formField.hasAttribute("data-placeholder-nohide")) {
+                formField.dataset.placeholder = formField.placeholder;
+            }
+        });
+    }
+    document.body.addEventListener("focusin", function (e) {
+        const targetElement = e.target;
+        if (targetElement.tagName === "INPUT" || targetElement.tagName === "TEXTAREA") {
+            if (targetElement.dataset.placeholder) {
+                targetElement.placeholder = "";
+            }
+            if (!targetElement.hasAttribute("data-no-focus-classes")) {
+                targetElement.classList.add("_form-focus");
+                targetElement.parentElement.classList.add("_form-focus");
+            }
+            formValidate.removeError(targetElement);
+        }
+    });
+    document.body.addEventListener("focusout", function (e) {
+        const targetElement = e.target;
+        if (targetElement.tagName === "INPUT" || targetElement.tagName === "TEXTAREA") {
+            if (targetElement.dataset.placeholder) {
+                targetElement.placeholder = targetElement.dataset.placeholder;
+            }
+            if (!targetElement.hasAttribute("data-no-focus-classes")) {
+                targetElement.classList.remove("_form-focus");
+                targetElement.parentElement.classList.remove("_form-focus");
+            }
+            // Моментальная валидация
+            if (targetElement.hasAttribute("data-validate")) {
+                formValidate.validateInput(targetElement);
+            }
+        }
+    });
+}
+// Валидация форм
+export let formValidate = {
+    getErrors(form) {
+        let error = 0;
+        let formRequiredItems = form.querySelectorAll("*[data-required]");
+        if (formRequiredItems.length) {
+            formRequiredItems.forEach((formRequiredItem) => {
+                if (
+                    (formRequiredItem.offsetParent !== null || formRequiredItem.tagName === "SELECT") &&
+                    !formRequiredItem.disabled
+                ) {
+                    error += this.validateInput(formRequiredItem);
+                }
+            });
+        }
+        return error;
+    },
+    validateInput(formRequiredItem) {
+        let error = 0;
+
+        if (!formRequiredItem.value) {
+            this.addError(formRequiredItem);
+            error++;
+        } else {
+            this.removeError(formRequiredItem);
+        }
+        return error;
+    },
+    addError(formRequiredItem) {
+        formRequiredItem.classList.add("_form-error");
+        formRequiredItem.parentElement.classList.add("_form-error");
+        let inputError = formRequiredItem.parentElement.querySelector(".form__error");
+        if (inputError) formRequiredItem.parentElement.removeChild(inputError);
+        if (formRequiredItem.dataset.error) {
+            formRequiredItem.parentElement.insertAdjacentHTML(
+                "beforeend",
+                `<div class="form__error">${formRequiredItem.dataset.error}</div>`
+            );
+        }
+    },
+    removeError(formRequiredItem) {
+        formRequiredItem.classList.remove("_form-error");
+        formRequiredItem.parentElement.classList.remove("_form-error");
+        if (formRequiredItem.parentElement.querySelector(".form__error")) {
+            formRequiredItem.parentElement.removeChild(
+                formRequiredItem.parentElement.querySelector(".form__error")
+            );
+        }
+    },
+    formClean(form) {
+        form.reset();
+        setTimeout(() => {
+            let inputs = form.querySelectorAll("input,textarea");
+            for (let index = 0; index < inputs.length; index++) {
+                const el = inputs[index];
+                el.parentElement.classList.remove("_form-focus");
+                el.classList.remove("_form-focus");
+                formValidate.removeError(el);
+            }
+        }, 0);
+    },
+    emailTest(formRequiredItem) {
+        return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(formRequiredItem.value);
+    },
+};
+/* Отправка форм */
+export function formSubmit(options = { validate: true }) {
+    const forms = document.forms;
+    if (forms.length) {
+        for (const form of forms) {
+            form.addEventListener("submit", function (e) {
+                const form = e.target;
+                formSubmitAction(form, e);
+            });
+            form.addEventListener("reset", function (e) {
+                const form = e.target;
+                formValidate.formClean(form);
+            });
+        }
+    }
+    async function formSubmitAction(form, e) {
+        const error = !form.hasAttribute("data-no-validate") ? formValidate.getErrors(form) : 0;
+        if (error === 0 && form.hasAttribute("data-dev")) {
+            // Если режим разработки
+            e.preventDefault();
+            formSent(form);
+        } else {
+            e.preventDefault();
+            const formError = form.querySelector("._form-error");
+            if (formError && form.hasAttribute("data-goto-error")) {
+                gotoBlock(formError, true, 1000);
+            }
+        }
+    }
+    // Действия после отправки формы
+    function formSent(form) {
+        // Создаем событие отправки формы
+        document.dispatchEvent(
+            new CustomEvent("formSent", {
+                detail: {
+                    form: form,
+                },
+            })
+        );
+        // Очищаем форму
+        formValidate.formClean(form);
+    }
+}
